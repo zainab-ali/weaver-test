589c30d1880115e704526e9b44e533a39958c81c
zainab-ali <zainab@kebab-ca.se>
2025-09-18 17:05:42 +0100

    Remove BaseIOSuite and BaseFunIOSuite.

--- modules/core-cats/shared/src/main/scala/weaver/Suites.scala
+++ modules/core-cats/shared/src/main/scala/weaver/Suites.scala
@@ -6,12 +6,18 @@ trait BaseCatsSuite extends EffectSuite.Provider[IO]
 
 abstract class MutableIOSuite
     extends MutableFSuite[IO]
-    with BaseIOSuite
-    with Expectations.Helpers
+    with BaseCatsSuite
+    with Expectations.Helpers {
+  implicit protected def effectCompat: UnsafeRun[IO] = CatsUnsafeRun
+  def getSuite: RunnableSuite[IO]                      = this
+}
 
 abstract class SimpleMutableIOSuite extends MutableIOSuite {
   type Res = Unit
   def sharedResource: Resource[IO, Unit] = Resource.pure[IO, Unit](())
 }
 
-trait FunSuiteIO extends BaseFunIOSuite with Expectations.Helpers
+trait FunSuiteIO extends FunSuiteF[IO] with BaseCatsSuite with Expectations.Helpers {
+  implicit protected def effectCompat: UnsafeRun[IO] = CatsUnsafeRun
+  def getSuite: RunnableSuite[IO]                              = this
+}
